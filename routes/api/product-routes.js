b0018751bbc747c442dd1bf60e1844a02fd07822
moyuh <mlroth94@gmail..com>
2022-09-17 20:21:56 -0600

    adds server fixes route issues

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include:[{ model: Tag, through: ProductTag, as: 'product_tags' }, { model: Category, as: 'products_category' }]
+      include:[{ model: Tag, through: ProductTag, as: 'product_tags' }, { model: Category }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -23,7 +23,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include:[{ model: Tag, through: ProductTag, as: 'product_tags' }, { model: Category, as: 'products_category' }]
+      include:[{ model: Tag, through: ProductTag, as: 'product_tags' }]
     });
     if(!productData) {
       res.status(404).json({ message: 'No product found with this "id"'});
